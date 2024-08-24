05945f3b0ad3c771318ec15bfaa5fce04f419b59
Waizul <waizul@gmail.com>
2024-08-24 22:32:57 +0600

    Initial setup vite with shadcn and tailwind css.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      assignment
+      <Button>assignment</Button>
+    </div>
+  );
+}
+
+export default App;
